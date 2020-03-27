e35af07f10e917e64832dcd0760a36ace8b09889
Qu3rid0 <andrey.oi@hotmail.com>
2020-03-27 20:01:40 -0300

    Create Encrypt_xor.cpp

---
+++ Encrypt_xor.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h> 
+
+void Decrypter(char encrypt[]) { 
+	char key = 22; //key
+	int script = strlen(encrypt); 
+	for (int i = 0; i < script; i++) { 
+		encrypt[i] = encrypt[i] ^ key;
+        printf("%c",encrypt[i]);  
+	} 
+} 
+
+int main() { 
+	char sampleString[] = 'example';
+	// Decrypt the string 
+	printf('Decrypted String: '); 
+	Decrypter(sampleString);
+    printf('\n'); 
+
+	return 0; 
+} 
