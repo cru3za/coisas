8f00c051c4062bb31472817905f5af44769729b9
cru3za <cru3za@gmail.com>
2020-05-18 19:04:22 -0300

    Update CriptografiaXor.cpp

--- CriptografiaXor.cpp
+++ CriptografiaXor.cpp
@@ -3,17 +3,22 @@
 #include<bits/stdc++.h>
 
 void Decrypter(char* Encrypt_Decrypt) { 
-    char Chave[6] = {'c','r','u','3','z','a'};
+    char Chave[2] = {4, 4}; //Coloque quantas chaves quiser
     for (int i = 0; i < (strlen(Encrypt_Decrypt)); i++){ 
         Encrypt_Decrypt[i] = Encrypt_Decrypt[i] ^ Chave[i % (sizeof(Chave) / sizeof(char))]; 
         printf("%c",Encrypt_Decrypt[i]); 
     } 
 } 
 int main() { 
-    char Cru3za[] = "www.github.com/cru3za";
+    char Cru3za[] = "www.github.com/cru3za"; //Texto a ser Criptografado
     printf("Encrypted String: "); 
     Decrypter(Cru3za); 
     printf("\n"); 
     printf("Decrypted String: "); 
     Decrypter(Cru3za);
 }
+
+
+// output
+// Encrypted String: sss*cmplqf*gki+gvq7~e
+// Decrypted String: www.github.com/cru3za
