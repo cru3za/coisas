125177f5b5ef9c3f761cfd0f2a4bfb1876b1ac52
Cru3za <cru3za@gmail.com>
2021-02-14 21:18:37 -0300

    Update and rename NomeHostParaIP.cpp to HostParaIP.cpp

--- HostParaIP.cpp
+++ HostParaIP.cpp
@@ -5,38 +5,31 @@
 #pragma comment(lib, "Ws2_32.lib")
 #define DEFAULT_BUFLEN 1024
 
-#pragma comment(lib,"ws2_32.lib")
-
 int main(int argc , char *argv[]){
 	WSADATA wsa;
-	char *hostname = "www.github.com";
+	char hostname[20];
+	//char *hostname = "www.github.com";
 	char ip[100];
 	struct hostent *he;
 	struct in_addr **addr_list;
 	int i;
 	
+	printf("\nInsira o dominio: ");
+	scanf("%s",&hostname);
 	printf("\nIniciando Winsock...");
 	if (WSAStartup(MAKEWORD(2,2),& wsa) != 0){
-		printf("Erro : %d",WSAGetLastError());
+		printf("\n Erro : %d",WSAGetLastError());
 		return 1;
 	}
-	
-	printf("Inicializado.\n");
-	
-		
+	printf("\nPerfeito, inicializado :)\n");
 	if ( (he = gethostbyname( hostname ) ) == NULL) {
-		printf("falha ao obter o nome do host : %d" , WSAGetLastError());
+		printf("\nFalha ao obter o nome do host : %d" , WSAGetLastError());
 		return 1;
 	}
-	
 	addr_list = (struct in_addr **) he-> h_addr_list;
-	
 	for(i = 0; addr_list[i] != NULL; i++) {
-		
 		strcpy(ip , inet_ntoa(*addr_list[i]) );
 	}
-	
-	printf("%s transformado para : %s\n" , hostname , ip);
-    
+	printf("Dominio -> [ %s ]\nIP -> [ %s ]\n\n" , hostname , ip);
 	return 0;
 }
